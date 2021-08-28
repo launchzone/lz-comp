f85c4fe20efad14c28de73058038af7735b02d1e
Hoang Hiep <hiep1337@gmail.com>
2021-08-28 17:18:53 +0700

    support icon for mockup

--- mockup/src/components/Menu/index.tsx
+++ mockup/src/components/Menu/index.tsx
@@ -17,10 +17,11 @@ export const Menu = ({
         if(item.children && item.children.length > 0) {
           SubMenu = <Menu menuConfig={item.children} className='sub-menu'/>
         }
-
+        const Icon = item.icon
         return <li className={`item ${location.pathname === item.path ? 'active' : '' }`} key={key}>
           {
             <Link to={item.path ? item.path : '#'}>
+              {Icon && <Icon className='menu-item-icon'/>}
               <span>{item.name}</span>
             </Link>
           }
